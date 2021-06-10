949050379cb2c82cacc2c5037582d778641c9955
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-10 10:11:51 +0200

    push exercices PHP partie01

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projet01 Exercice01</title>
+</head>
+<body>
+    <h1>PHP Projet01 Exercice01</h1>
+    <h2>Créez une variable et lui affecter une chaîne de caractères. Afficher cette variable.</h2>
+    <?php
+        $maVariable = 'Dear lovely';
+        echo $maVariable;
+    ?>    
+    <h2><?= $maVariable?></h2>
+</body>
+</html>
