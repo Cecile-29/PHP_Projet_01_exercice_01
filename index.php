e65fce78204a1b5d14aeb7ac92c8af918f79670a
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-14 16:27:16 +0200

    push exercices PHP

--- index.php
+++ index.php
@@ -5,14 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Projet01 Exercice01</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>PHP Projet01 Exercice01</h1>
-    <h2>Créez une variable et lui affecter une chaîne de caractères. Afficher cette variable.</h2>
+    <div class="container-fluid col-7 p-3 m-4 border border-warning rounded justify-content-center">
+        <div class="row ">
+            <h1 class="col-12 text-secondary text-center">PHP Projet01 Exercice01</h1>
+            <h3 class="col-12 text-info text-center">Créez une variable et lui affecter une chaîne de caractères. Afficher cette variable.</h3>
+        </div>
+        <div class="row border border-primary rounded p-3 m-4 text-secondary text-center">
     <?php
-        $maVariable = 'Dear lovely';
+        $maVariable = 'Dear lovely !';
         echo $maVariable;
-    ?>    
-    <h2><?= $maVariable?></h2>
+    ?> 
+        </div>
+    </div> 
 </body>
 </html>
